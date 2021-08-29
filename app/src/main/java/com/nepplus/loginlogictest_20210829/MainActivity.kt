57dec5b14bfca7d2e232f09c1848fa5e487288fc
김다은 <softeun92@naver.com>
2021-08-29 13:32:24 +0900

    ui 작업현황

--- app/src/main/java/com/nepplus/loginlogictest_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/loginlogictest_20210829/MainActivity.kt
@@ -2,10 +2,24 @@ package com.nepplus.loginlogictest_20210829
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        LoginBtn.setOnClickListener {
+
+            val idTxt = "admin"
+            val passTxt
+
+            Toast.makeText(this, "관리자님 환영 합니다.", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
