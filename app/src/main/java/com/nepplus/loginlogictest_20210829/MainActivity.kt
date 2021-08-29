1e941e284bf209fb5ac000ac67abfc2cb2f170ac
김다은 <softeun92@naver.com>
2021-08-29 13:39:03 +0900

    아이디 / 비번 확인 (if)   - 둘다 맞아야함 ( AND 연산 -&&)

--- app/src/main/java/com/nepplus/loginlogictest_20210829/MainActivity.kt
+++ app/src/main/java/com/nepplus/loginlogictest_20210829/MainActivity.kt
@@ -11,12 +11,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        //  로그인 버튼 눌렸을 때 이벤트 처리
+
         LoginBtn.setOnClickListener {
 
-            val idTxt = "admin"
-            val passTxt
+           // 입력한 아이디를 받아서 저장
+            val inputId = idEdt.text.toString()
 
-            Toast.makeText(this, "관리자님 환영 합니다.", Toast.LENGTH_SHORT).show()
+            //입력한 비밀번호도 받아서 저장
+
+            val inputPw = PasswordEdt.text.toString()
+
+
+            // 아이디/비번에 저장된 값이 뭔지 질문.  if 문
+
+            if (inputId == "admin" && inputPw == "qwer"){
+
+            //     아이디 /비번이 둘다 맞는경우
+
+
+
+            Toast.makeText(this, "관리자님 환영 합니다.", Toast.LENGTH_SHORT).show()}
+            
+            else {
+
+                Toast.makeText(this, "로그인에 실패했습니", Toast.LENGTH_SHORT).show()
+            }
         }
 
 
